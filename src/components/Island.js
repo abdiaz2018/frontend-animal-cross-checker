cefdc84a9318869e82624d1b316c97c3cd74bb16
Abraham <abdiaz2018@gmail.com>
2021-10-14 19:45:38 -0700

    added island component

---
+++ src/components/Island.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+
+class Island extends React.Component {
+
+
+    render () {
+        return (
+            <div className="ui raised segment">
+                <h3>Name:</h3>
+                <h5>Villagers:</h5>
+                
+            </div>
+        )
+    }
+}
+
+export default Island;
