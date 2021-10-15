cee18b1c636d54e95e08d7645b32d68a964154b2
Abraham <abdiaz2018@gmail.com>
2021-10-14 19:46:18 -0700

    added redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { Provider } from "react-redux"
+import {createStore} from "redux"
+import trackerReducer from './reducers/trackerReducer';
+
+
+const store = createStore(trackerReducer)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
