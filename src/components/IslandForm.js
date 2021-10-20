18e9a10f36180dc566dc3eed9f1b35cbd3e26a59
Abraham <abdiaz2018@gmail.com>
2021-10-19 20:49:17 -0700

    added villager fields

--- src/components/IslandForm.js
+++ src/components/IslandForm.js
@@ -27,6 +27,87 @@ class IslandForm extends React.Component {
                     onChange={this.handleFormChange}
                     value={this.state.islandName}
                     />
+                    <label>First Villager</label>
+                    <input 
+                    type="text" 
+                    name="villager1"
+                    placeholder="Villager name..."
+                    onChange={this.handleFormChange}
+                    value={this.state.villager1}
+                    />
+                    <label>Second Villager</label>
+                    <input 
+                    type="text" 
+                    name="villager2"
+                    placeholder="Villager name..."
+                    onChange={this.handleFormChange}
+                    value={this.state.villager2}
+                    />
+                    <label>Third Villger</label>
+                    <input 
+                    type="text" 
+                    name="villager3"
+                    placeholder="Villager name..."
+                    onChange={this.handleFormChange}
+                    value={this.state.villager3}
+                    />
+                    <label>Fourth Villager</label>
+                    <input 
+                    type="text" 
+                    name="villager4"
+                    placeholder="Villager name..."
+                    onChange={this.handleFormChange}
+                    value={this.state.villager4}
+                    />
+                    <label>Fifth Villager</label>
+                    <input 
+                    type="text" 
+                    name="villager5"
+                    placeholder="Villager name..."
+                    onChange={this.handleFormChange}
+                    value={this.state.villager5}
+                    />
+                    <label>Sixth Villager</label>
+                    <input 
+                    type="text" 
+                    name="villager6"
+                    placeholder="Villager name..."
+                    onChange={this.handleFormChange}
+                    value={this.state.villager6}
+                    />
+                    <label>Seventh Villager</label>
+                    <input 
+                    type="text" 
+                    name="villager7"
+                    placeholder="Villager name..."
+                    onChange={this.handleFormChange}
+                    value={this.state.villager7}
+                    />
+                    <label>Eighth Villager</label>
+                    <input 
+                    type="text" 
+                    name="villager8"
+                    placeholder="Villager name..."
+                    onChange={this.handleFormChange}
+                    value={this.state.villager8}
+                    />
+                    <label>Ninth Villager</label>
+                    <input 
+                    type="text" 
+                    name="villager9"
+                    placeholder="Villager name..."
+                    onChange={this.handleFormChange}
+                    value={this.state.villager9}
+                    />
+                    <label>Last Villager</label>
+                    <input 
+                    type="text" 
+                    name="villager10"
+                    placeholder="Villager name..."
+                    onChange={this.handleFormChange}
+                    value={this.state.villager10}
+                    />
+                    <input type="submit" />
                 </form>
             </div>
         )
