ea76d2738f7075fdc62b397ff01cc835c8fcb61f
Abraham <abdiaz2018@gmail.com>
2021-10-19 20:43:16 -0700

    dropdown class added

---
+++ src/components/VillagerDropdown.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class VillagerDropdown extends React.Component {
+
+
+    render() {
+        return (
+            <select
+            className="ui selection dropdown"
+            name="villagers">    
+            </select>
+        )
+    }
+}
+
+export default VillagerDropdown;
