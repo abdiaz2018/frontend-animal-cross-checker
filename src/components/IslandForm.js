790af30ddda55a2ebaf5fe389ee24836a80dd8e1
Abraham <abdiaz2018@gmail.com>
2021-10-19 20:46:50 -0700

    added form

--- src/components/IslandForm.js
+++ src/components/IslandForm.js
@@ -3,6 +3,15 @@ import React from "react";
 class IslandForm extends React.Component {
 
 
+    render () {
+        return (
+            <div>
+                <h3>Create your Island below!</h3>
+                <form className="ui form" onSubmit={this.handleSubmit}>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default IslandForm;
