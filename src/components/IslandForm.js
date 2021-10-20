ebab67ccf9ae2dc0058c3383908bc4874e1fa729
Abraham <abdiaz2018@gmail.com>
2021-10-19 20:47:46 -0700

    added island name field

--- src/components/IslandForm.js
+++ src/components/IslandForm.js
@@ -2,12 +2,22 @@ import React from "react";
 
 class IslandForm extends React.Component {
 
+    state = {
+        islandName: "",
+    }
 
     render () {
         return (
             <div>
                 <h3>Create your Island below!</h3>
                 <form className="ui form" onSubmit={this.handleSubmit}>
+                    <input 
+                    type="text" 
+                    name="islandName"
+                    placeholder="Island name..."
+                    onChange={this.handleFormChange}
+                    value={this.state.islandName}
+                    />
                 </form>
             </div>
         )
