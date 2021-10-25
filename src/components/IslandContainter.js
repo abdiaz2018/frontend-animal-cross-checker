8ab43ce838933704789008e332e93e94de2d4f7b
Abraham <abdiaz2018@gmail.com>
2021-10-25 16:39:45 -0700

    adding switch checkbox method

--- src/components/IslandContainter.js
+++ src/components/IslandContainter.js
@@ -12,17 +12,21 @@ class IslandContainter extends React.Component {
         fetch("http://localhost:3000/islands")
         .then(res => res.json())
         .then(json => {
-            console.log(json)
             this.setState({
                 islands: json
             })
         })
     }
-    
-    displayIslands(){
-        return this.state.islands.map(island => <Island island={island} id={island.id} name={island.name} fossil={island.fossil} glowing_spot={island.glowing_spot} nooks_cranny={island.nooks_cranny} able_shop={island.able_shop} shake_trees={island.shake_trees} collect_fruit={island.collect_fruit}/>)
-    }
 
+    displayIslands(){
+        return this.state.islands.map(island => <Island switchCheckbox={this.switchCheckbox} id={island.id} name={island.name} fossils={island.fossils} glowing_spot={island.glowing_spot} nooks_cranny={island.nooks_cranny} able_shop={island.able_shop} shake_trees={island.shake_trees} collect_fruit={island.collect_fruit}/>)
+    }
+    
+    switchCheckbox = (e) => {
+        console.log(e.target.name)
+        
+    }
+    
 
     render() {
         return (
