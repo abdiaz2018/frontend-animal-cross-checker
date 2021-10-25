6b17e5dd6cd07b43afa36d3c69bd68bfe4e60665
Abraham <abdiaz2018@gmail.com>
2021-10-25 09:11:55 -0700

    adding display islands function

--- src/components/IslandContainter.js
+++ src/components/IslandContainter.js
@@ -6,6 +6,7 @@ class IslandContainter extends React.Component {
     state = {
         islands: []
     }
+
     componentDidMount(){
         // const url = "http://localhost:3000/islands"
         fetch("http://localhost:3000/islands")
@@ -17,6 +18,12 @@ class IslandContainter extends React.Component {
             })
         })
     }
+    
+    displayIslands(){
+        return this.state.islands.map(island => <Island island={island} id={island.id} name={island.name} fossil={island.fossil} glowing_spot={island.glowing_spot} nooks_cranny={island.nooks_cranny} able_shop={island.able_shop} shake_trees={island.shake_trees} collect_fruit={island.collect_fruit}/>)
+    }
+
+
     render() {
         return (
             <div>
