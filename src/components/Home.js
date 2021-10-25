f2b3713b83d192eaa86e5a02af65935f1cd7fbeb
Abraham <abdiaz2018@gmail.com>
2021-10-25 10:16:32 -0700

    created home component

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+
+    return(
+        <div>
+            <h1>Welcome to my Animal Crossing Daily Checklist!</h1>
+            <h4>Click New Island above to create your island checklist or click my islands if you made one already</h4>
+        </div>
+    )
+}
+
+export default Home;
