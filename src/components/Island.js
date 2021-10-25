22ac92e7e04099e385eeb7425bf24e57547e9abd
Abraham <abdiaz2018@gmail.com>
2021-10-25 16:38:11 -0700

    adding checkboxes

--- src/components/Island.js
+++ src/components/Island.js
@@ -7,15 +7,14 @@ function Island(props) {
     return(
         <div className="ui container segment" id={`island-${props.id}`}>
             <h1>{props.name} Daily Checklist:</h1>
-            <h3>Found Fossils</h3>
-            <h3>Found Glowing Spot</h3>
-            <h3>Checked Able Shop</h3>
-            <h3>Checked Nook's Cranny</h3>
-            <h3>Collected Fruit</h3>
-            <h3>Tree Shook</h3>
+            <h3><input type="checkbox" onClick={props.switchCheckbox} name="glowing_spot" checked={props.glowing_spot}/>Found Glowing Spot</h3>
+            <h3><input type="checkbox" onClick={props.switchCheckbox} name="fossils" checked={props.fossils}/>Found Fossils</h3>
+            <h3><input type="checkbox" onClick={props.switchCheckbox} name="able_shop" checked={props.able_shop}/>Checked Able Shop</h3>
+            <h3><input type="checkbox" onClick={props.switchCheckbox} name="nooks_cranny" checked={props.nooks_cranny}/>Checked Nook's Cranny</h3>
+            <h3><input type="checkbox" onClick={props.switchCheckbox} name="collect_fruit" checked={props.collect_fruit}/>Collected Fruit</h3>
+            <h3><input type="checkbox" onClick={props.switchCheckbox} name="shake_trees" checked={props.shake_trees}/>Tree Shook</h3>
             <h3>Villagers talked to:</h3>
-            {console.log(props.villagers)}
-            <VillagerContainer />
+            <VillagerContainer island_id={props.id}/>
         </div>
     )
 }
