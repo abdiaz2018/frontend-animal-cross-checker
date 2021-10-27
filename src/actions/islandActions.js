2ff09910f86a7e0e31469d98c868432055cc2cb7
Abraham <abdiaz2018@gmail.com>
2021-10-26 19:22:44 -0700

    moving get fetch request

--- src/actions/islandActions.js
+++ src/actions/islandActions.js
@@ -3,3 +3,15 @@ const url = "http://localhost:3000/islands"
 export const addIsland = (island) => ({ type: "ADD_ISLAND", payload: island});
 
 export const getIslands = (islands) => ({type: "GET_ISLANDS", payload: islands});
+
+
+export const fetchIslands = () => {
+
+    return (dispatch) => {
+        fetch(url)
+        .then(res => res.json())
+        .then(json => {
+            dispatch(getIslands(json))
+        })
+    }
+}
